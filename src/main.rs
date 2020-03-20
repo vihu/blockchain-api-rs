bcde9ed84165c992dfe74a63fe0b9789b1b6a3cf
Rahul Garg <rg@vihu.dev>
2020-03-20 01:41:58 -0700

    Raw sqlx appears to work

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,39 @@ pub struct Block {
     pub rescue_signature: String,
 }
 
-#[async_std::main]
-async fn main() -> anyhow::Result<()> {
+async fn db_pool() -> anyhow::Result<PgPool> {
     dotenv().ok();
-    let mut pool = PgPool::new(&env::var("DATABASE_URL")?).await?;
+    let pool = PgPool::new(&env::var("DATABASE_URL")?).await?;
+    Ok(pool)
+}
 
+async fn list_blocks(pool: &mut PgPool) -> anyhow::Result<Vec<Block>> {
     let blocks = sqlx::query_as!(Block,
-        "select * from blocks order by height desc limit 2")
-        .fetch_all(&mut pool)
+        "select * from blocks order by height desc")
+        .fetch_all(pool)
         .await?;
 
+    Ok(blocks)
+}
+
+async fn get_block(pool: &mut PgPool, height: i64) -> anyhow::Result<Block> {
+    let block = sqlx::query_as!(Block,
+        "SELECT * FROM blocks WHERE height = $1", height)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(block)
+}
+
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+
+    let mut pool = db_pool().await?;
+    let blocks = list_blocks(&mut pool).await?;
+    let block = get_block(&mut pool, 30).await?;
+
     println!("blocks: {:?}", blocks);
+    println!("block_30: {:?}", block);
 
     Ok(())
 }
